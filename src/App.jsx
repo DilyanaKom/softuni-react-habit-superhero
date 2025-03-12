c61ff0f91ff9eac6d2dc842dcce7f076cf609487
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-03-12 10:10:46 +0200

    Extract Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,17 @@
 import './css/bootstrap.min.css';
 import './css/templatemo-style.css';
+
+import Navigation from './components/Navigation';
+
+
 function App() {
 
 
   return (
     <>
-    <nav className="navbar navbar-expand-lg">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-                <i className="fas fa-film mr-2"></i>
-                Habit SuperHero
-            </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <i className="fas fa-bars"></i>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                    <a className="nav-link nav-link-1 active" aria-current="page" href="index.html">Photos</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link nav-link-2" href="videos.html">Videos</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link nav-link-3" href="about.html">About</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link nav-link-4" href="contact.html">Contact</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-    </nav>
+    <Navigation />
 
-    <div className="tm-hero d-flex justify-content-center align-items-center" data-parallax="scroll" data-image-src="img/hero.jpg">
+    <div className="tm-hero d-flex justify-content-center align-items-center" data-parallax="scroll" data-image-src="/hero.jpg">
         <form className="d-flex tm-search-form">
             <input className="form-control tm-search-input" type="search" placeholder="Search" aria-label="Search"/>
             <button className="btn btn-outline-success tm-search-btn" type="submit">
---
+++ src/components/NavigationLink.jsx
@@ -0,0 +1,7 @@
+export default function NavigationLink(){
+    return (
+        <li className="nav-item">
+        <a className="nav-link nav-link-1 active" aria-current="page" href="index.html">Photos</a>
+    </li>
+    )
+}
