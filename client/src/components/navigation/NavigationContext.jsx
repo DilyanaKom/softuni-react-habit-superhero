4ca607fcbc3da3a16d68b96641e95552ec849a22
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-03-17 20:08:52 +0200

    Add create challenge link in navigation

--- client/src/components/navigation/NavigationContext.jsx
+++ client/src/components/navigation/NavigationContext.jsx
@@ -5,13 +5,13 @@ const NavigationContext = createContext([]);
 export const NavigationProvider = ( {children}) => {
     const navLinks = [
         {id: 'home', name: 'Home', path:'/', className: 'navLink1' },
-        {id: 'catalog', name: 'Catalog', path:'/catalog', className: 'navLink2' },
+        {id: 'challenges', name: 'Challenges', path:'/challenges', className: 'navLink2' },
         {id: 'register', name: 'Register', path:'/register', className: 'navLink3' },
         {id: 'profile', name: 'Profile', path:'/profile', className: 'navLink4' },
         {id: 'login', name: 'Login', path:'/login', className: 'navLink5' },
         {id: 'logout', name: 'Logout', path:'/logout', className: 'navLink6' },
-        
-        
+        {id: 'createChallenge', name: 'Create', path:'/challenges/create', className: 'navLink7' },
+          
     ];
 
     return (
