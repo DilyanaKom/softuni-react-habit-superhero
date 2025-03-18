ff8262bf669a40f960f9a5c749373b952b2831a3
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-03-18 19:54:32 +0200

    Add active nav link style

--- client/src/components/navigation/NavigationLink.jsx
+++ client/src/components/navigation/NavigationLink.jsx
@@ -1,10 +1,18 @@
-import { Link } from 'react-router';
+import { Link, NavLink } from 'react-router';
 import styles from './NavigationLink.module.css'
 
 export default function NavigationLink({name, path, className}){
     return (
         <li className="nav-item">
-        <Link to={path} className={`nav-link ${styles.navLink} ${styles[className]}`} >{name}</Link>
+        {/* <NavLink to={path} className={`nav-link ${styles.navLink} ${styles[className]}`} >{name}</NavLink> */}
+        <NavLink 
+        to={path}
+        className={ ({isActive}) =>
+            `nav-link ${styles.navLink} ${styles[className]} ${isActive ? styles.active : ''}`
+        }
+
+        >{name}</NavLink>
+
     </li>
     )
 }
