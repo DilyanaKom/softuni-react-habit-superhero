659913bb883510865ae1519c3cba9f5823694c0e
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-03-24 11:03:47 +0200

    Add spinner while logging out

--- client/src/components/user/Logout.jsx
+++ client/src/components/user/Logout.jsx
@@ -1,29 +1,33 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect} from "react"
 import styles from "../Spinner.module.css"
 import { useLogout } from "../../api/authApi"
 import { useNavigate } from "react-router";
 import { UserContext } from "./UserContext";
 
 
-export default function Logout(){
-    const {logout} = useLogout();
-    const {userLoginHandler} = useContext(UserContext);
+export default function Logout() {
+    const { logout } = useLogout();
+    const { userLoginHandler } = useContext(UserContext);
     const navigate = useNavigate();
 
     useEffect(() => {
         logout()
-        .then(
-            userLoginHandler({})
-        );
+            .then(() => {
+                userLoginHandler({});
+                navigate('/');
+            })
+            .catch((error) => {
+                console.log(error.message)
+            })
 
     }, [])
 
-return (
-    <div className={styles.spinnerContainer}>
-      <div className={styles.spinner}></div>
-      <p className={styles.loadingText}>Logging out...</p>
-    </div>
-)
+    return (
+        <div className={styles.spinnerContainer}>
+            <div className={styles.spinner}></div>
+            <p className={styles.loadingText}>Logging out...</p>
+        </div>
+    )
 
 
 }
