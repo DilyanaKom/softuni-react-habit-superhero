750afabe98b37f281de8869cd4992ef2c907ca6e
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-03-26 15:03:14 +0200

    Add edit challenge component

---
+++ client/src/components/challenges/EditChallenge.jsx
@@ -0,0 +1,79 @@
+import { useParams } from 'react-router';
+import styles from '../Forms.module.css'
+import { useChallenges } from '../../api/challengeApi';
+
+export default function EditChallenge() {
+    const {challengeId} = useParams();
+    const {currentChallenge} = useChallenges(challengeId);
+    
+
+
+    return (
+      <div className={styles.registrationContainer}>
+        <div className={styles.registrationCard}>
+          <h2 className={`${styles.textPrimary} ${styles.mb5}`}>Edit Challenge</h2>
+          <form id="create-challenge-form" className={styles.contactForm}>
+            <div className={styles.formGroup}>
+              <label htmlFor="title">Challenge Title</label>
+              <input
+                type="text"
+                id="title"
+                name="title"
+                className={styles.formControl}
+                placeholder="Challenge Title"
+                defaultValue={currentChallenge?.title}
+                required
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="duration">Duration (days)</label>
+              <input
+                type="number"
+                id="duration"
+                name="duration"
+                className={styles.formControl}
+                placeholder="Duration (days)"
+                defaultValue={currentChallenge?.duration}
+                required
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="description">Challenge Description</label>
+              <textarea
+                id="description"
+                name="description"
+                className={styles.formControl}
+                placeholder="Challenge Description"
+                required
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="mediaUrl">Image/Video URL</label>
+              <input
+                type="text"
+                id="mediaUrl"
+                name="mediaUrl"
+                className={styles.formControl}
+                placeholder="Image/Video URL"
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="difficulty">Difficulty Level</label>
+              <select id="difficulty" name="difficulty" className={styles.formControl} required>
+                <option value="">Select Difficulty Level</option>
+                <option value="easy">Easy</option>
+                <option value="medium">Medium</option>
+                <option value="hard">Hard</option>
+              </select>
+            </div>
+            <div className={styles.formGroup}>
+              <button type="submit" className={styles.btnPrimary}>
+                Edit Challenge
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+    
+}
