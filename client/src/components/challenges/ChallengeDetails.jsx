5d5ebeee46041edc837d914a8b382b9847c8b07b
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-03-26 11:44:54 +0200

    Hide buttons for guests

--- client/src/components/challenges/ChallengeDetails.jsx
+++ client/src/components/challenges/ChallengeDetails.jsx
@@ -1,24 +1,30 @@
+import { useContext } from "react";
 import styles from "./ChallengeDetails.module.css";
+import { UserContext } from "../user/UserContext";
 
 
-export default function ChallengeDetails({ challenge }){
+export default function ChallengeDetails({ challenge }) {
+
+    const { _id } = useContext(UserContext);
+    
     return (
         <div className={styles.detailsContainer}>
-          <h1 className={styles.challengeTitle}>Test</h1>
-          <div className={styles.detailsContent}>
-            <div className={styles.mediaDisplay}>Media Placeholder</div>
-            <div className={styles.infoSection}>
-              <p><strong>Author:</strong> Some Author</p>
-              <p><strong>Created On:</strong> 26 March 2025</p>
-              <p><strong>Duration:</strong> 10</p>
-              <p className={styles.description}><strong>Description:</strong> This is test</p>
-              <div className={styles.buttonGroup}>
-                <button className={styles.editButton}>Edit</button>
-                <button className={styles.deleteButton}>Delete</button>
-                <button className={styles.joinButton}>Join</button>
-              </div>
+            <h1 className={styles.challengeTitle}>Test</h1>
+            <div className={styles.detailsContent}>
+                <div className={styles.mediaDisplay}>Media Placeholder</div>
+                <div className={styles.infoSection}>
+                    <p><strong>Author:</strong> Some Author</p>
+                    <p><strong>Created On:</strong> 26 March 2025</p>
+                    <p><strong>Duration:</strong> 10</p>
+                    <p className={styles.description}><strong>Description:</strong> This is test</p>
+                    {_id && <div className={styles.buttonGroup}>
+                        <button className={styles.editButton}>Edit</button>
+                        <button className={styles.deleteButton}>Delete</button>
+                        <button className={styles.joinButton}>Join</button>
+                    </div>}
+
+                </div>
             </div>
-          </div>
         </div>
-      );
-  };
+    );
+};
