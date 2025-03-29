476fa074520f766a6a4d0fcde3d0c4c86642ee1f
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-03-29 16:31:35 +0200

    Update serverError.js

--- client/src/utils/serverError.js
+++ client/src/utils/serverError.js
@@ -1,7 +1,7 @@
 export default function handleServerError(status, result) {
     const statusCodes = {
-        400: "Please check the data sent.",
-        401: "Unauthorized. Please log in.",
+        400: "Something went wrong with your request.",
+        401: "Please log in.",
         403: "Incorrect email or password.",
         404: "The resource you requested does not exist.",
         409: "Resource already exists.",
