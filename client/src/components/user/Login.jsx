791d4617f1fc7eba51b06af241d40fb4118606c5
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-03-29 10:41:56 +0200

    Add display message upon error in login

--- client/src/components/user/Login.jsx
+++ client/src/components/user/Login.jsx
@@ -5,11 +5,12 @@ import styles from '../Forms.module.css'
 
 import { useLogin } from '../../api/authApi'
 import { UserContext } from './UserContext';
+import ErrorNotification from '../errors/ErrorNotification';
 
 export default function Login(){
     //TODO error handling
 
-    const { login } = useLogin();
+    const { login, error, clearError } = useLogin();
     const { userLoginHandler } = useContext(UserContext)
     const navigate = useNavigate();
     
@@ -17,8 +18,10 @@ export default function Login(){
         const { email, password} = Object.fromEntries(formData);
         
         const authData = await login(email, password);
+        if(!authData){
+            return;
+        }
         userLoginHandler(authData);
-
         navigate('/')
 
         
@@ -42,6 +45,7 @@ export default function Login(){
             <button type="submit" className={styles.btnPrimary} disabled={isPending}>Login</button>
         </div>
     </form>
+    <ErrorNotification error={error} onClear={clearError}/>
     <div className={`${styles.mt4} ${styles.textCenter}`}>
         <p>Don't have an account? <Link to="/register" className={styles.loginLink}>Register</Link></p>
     </div>                
