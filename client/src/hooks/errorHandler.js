c2b4295094d28d6982a5767290ac5622784628ba
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-03-29 06:51:16 +0200

    Extract error handling logic

--- client/src/hooks/errorHandler.js
+++ client/src/hooks/errorHandler.js
@@ -1,5 +1,13 @@
 import { useState } from "react";
 
 export const useErrorHandler = () => {
+    const [error, setError] = useState(null);
+
+    const clearError = () => setError(null);
     
+    return{
+        error,
+        setError,
+        clearError
+    }
 }
