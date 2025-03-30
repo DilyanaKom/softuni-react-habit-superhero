f4bd524df78c9528bc8a6d0f81b513288a8669f0
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-03-30 08:29:46 +0300

    Update CatalogItem.jsx

--- client/src/components/challenges/CatalogItem.jsx
+++ client/src/components/challenges/CatalogItem.jsx
@@ -1,22 +1,31 @@
 import { Link } from "react-router";
 
+import styles from './CatalogItem.module.css';
+
 import convertDate from "../../utils/convertDate"
 
-export default function CatalogItem({challenge}){
-    const createdOn = convertDate(challenge?._createdOn);
-    return (
-        <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5">
-        <figure className="effect-ming tm-video-item">
-          <img src={challenge?.mediaLink} alt="Image" className="img-fluid" />
-          <figcaption className="d-flex align-items-center justify-content-center">
-            <h2>{challenge?.title}</h2>
-            <Link to={`/challenges/${challenge?._id}/details`}>View more</Link>
-          </figcaption>
-        </figure>
-        <div className="d-flex justify-content-between tm-text-gray">
-          <span className="tm-text-gray-light">{createdOn}</span>
-          <span>By {challenge?.author.username}</span>
+export default function CatalogItem({ challenge }) {
+  const createdOn = convertDate(challenge?._createdOn);
+
+  return (
+    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5">
+      <figure className={`effect-ming tm-video-item ${styles.figureContainer}`}>
+        <div className={styles.imageWrapper}>
+          <img
+            src={challenge?.mediaLink}
+            alt="Image"
+            className={`img-fluid ${styles.catalogImage}`}
+          />
         </div>
+        <figcaption className="d-flex align-items-center justify-content-center">
+          <h2>{challenge?.title}</h2>
+          <Link to={`/challenges/${challenge?._id}/details`}>View more</Link>
+        </figcaption>
+      </figure>
+      <div className="d-flex justify-content-between tm-text-gray">
+        <span className="tm-text-gray-light">{createdOn}</span>
+        <span>By {challenge?.author.username}</span>
       </div>
-    )
+    </div>
+  );
 }
